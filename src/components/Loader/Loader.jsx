6fd6e7bfd14f4d64b89084492aba7cdf29e59b86
KorotinI <shobviponimali@gmail.com>
2024-05-02 16:13:46 +0200

    addLoader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,19 @@
-import { Comment } from "react-loader-spinner";
+import { BallTriangle } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <Comment
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="comment-loading"
-        wrapperStyle={{}}
-        wrapperClass="comment-wrapper"
-        color="#fff"
-        backgroundColor="#84336f"
-      />
+      <BallTriangle
+  height={100}
+  width={100}
+  radius={5}
+  color="#3aff24aa"
+  ariaLabel="ball-triangle-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  />
     </div>
   );
 };
